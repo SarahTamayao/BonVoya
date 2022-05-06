272b6ca7e3cc5e10e10101f89e5c11554c040f2d
Anthony Makaj <tmakaj213@gmail.com>
2022-05-06 18:42:30 -0400

    Implemented locality retrieval and changed welcome screen vector color to yellow

--- BonVoya/ViewController.swift
+++ BonVoya/ViewController.swift
@@ -5,15 +5,24 @@
 
 import UIKit
 import Parse
+import MapKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, CLLocationManagerDelegate {
     @IBOutlet weak var tableView: UITableView!
     
-
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+            locationManager.startUpdatingLocation()
+        }
         
         //This will tell the table view that the delegate and data source is present in the current view controller
         tableView.dataSource = self
@@ -23,11 +32,27 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         self.navigationController?.setNavigationBarHidden(true, animated: true)
     }
     
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        print("Locations = \(locValue.latitude) \(locValue.longitude)")
+        let geoCoder = CLGeocoder()
+        let location = CLLocation(latitude: locValue.latitude, longitude: locValue.longitude) // <- New York
+        
+        geoCoder.reverseGeocodeLocation(location, completionHandler: { (placemarks, _) -> Void in
+            placemarks?.forEach { (placemark) in
+                if let city = placemark.locality
+                {
+                    print(city)
+                }
+            }
+        })
+    }
+    
     //This function returns the number of table cells to show (this changes dynamically for each place
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 50
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         
@@ -35,15 +60,15 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         return cell
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
