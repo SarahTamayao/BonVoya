0eab2798ecc7f07a34f6339be3432a4402a35fe0
Anthony Makaj <tmakaj213@gmail.com>
2022-05-18 22:35:56 -0400

    Added structs to PlacesManager

--- BonVoya/PlacesManager.swift
+++ BonVoya/PlacesManager.swift
@@ -9,8 +9,6 @@ import Foundation
 import CoreLocation
 
 class PlacesManager {
-    
-
     func getCoordinateFrom(address: String, completion: @escaping(_ coordinate: CLLocationCoordinate2D?, _ error: Error?) -> () ) {
         CLGeocoder().geocodeAddressString(address) { completion($0?.first?.location?.coordinate, $1) }
         let address = "Rio de Janeiro, Brazil"
@@ -21,12 +19,21 @@ class PlacesManager {
             DispatchQueue.main.async {
                 print(address, "Location:", coordinate) // Rio de Janeiro, Brazil Location: CLLocationCoordinate2D(latitude: -22.9108638, longitude: -43.2045436)
             }
-
         }
     }
-    
-    
 }
 
-    
-    
+struct Entry: Codable {
+    let results: [Result]
+}
+
+struct Result: Codable {
+    let id, name: String
+    let location: Location
+    let types: [String]
+    let distance: Int
+}
+
+struct Location: Codable {
+    let lat, lng: Double
+}
