ea715b117e730587abed7d61d6396bdfc8b4f2e1
fizametla <fmetla@fordham.edu>
2022-05-18 22:22:15 -0400

    Updated city to Coordinates in places Manager

--- BonVoya/PlacesManager.swift
+++ BonVoya/PlacesManager.swift
@@ -9,11 +9,13 @@ import Foundation
 import CoreLocation
 
 class PlacesManager {
+    
 
     func getCoordinateFrom(address: String, completion: @escaping(_ coordinate: CLLocationCoordinate2D?, _ error: Error?) -> () ) {
-                CLGeocoder().geocodeAddressString(address) { completion($0?.first?.location?.coordinate, $1) }
-            let address = "Rio de Janeiro, Brazil"
-                getCoordinateFrom(address: address) { coordinate, error in
+        CLGeocoder().geocodeAddressString(address) { completion($0?.first?.location?.coordinate, $1) }
+        let address = "Rio de Janeiro, Brazil"
+
+        getCoordinateFrom(address: address) { coordinate, error in
             guard let coordinate = coordinate, error == nil else { return }
             // don't forget to update the UI from the main thread
             DispatchQueue.main.async {
@@ -25,3 +27,6 @@ class PlacesManager {
     
     
 }
+
+    
+    
