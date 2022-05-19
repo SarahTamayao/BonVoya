223612489ba6d23f4a4baf92493734e46f2c39f5
Anthony Makaj <tmakaj213@gmail.com>
2022-05-19 01:17:40 -0400

    Integrated PlacesManagers with TrueWay Places API

--- BonVoya/PlacesManager.swift
+++ BonVoya/PlacesManager.swift
@@ -10,69 +10,10 @@ import CoreLocation
 import Alamofire
 
 class PlacesManager {
-    
-    //API Places Caller
-//    func getNearbyAPI(coordinate: CLLocationCoordinate2D, completion: @escaping () -> ()) {
-//        let headers = [
-//            "X-RapidAPI-Host": "trueway-places.p.rapidapi.com",
-//            "X-RapidAPI-Key": "d21d23c616msh0dfcc69840c5774p1c3012jsne9d41c7837b3"
-//        ]
-//
-//        let request = NSMutableURLRequest(url: NSURL(string: "https://trueway-places.p.rapidapi.com/FindPlacesNearby?location=\(coordinate.latitude)%2C\(coordinate.longitude)&radius=10000&language=en")! as URL,cachePolicy: .useProtocolCachePolicy, timeoutInterval: 10.0)
-//        request.httpMethod = "GET"
-//        request.allHTTPHeaderFields = headers
-//
-//        let session = URLSession.shared
-//        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
-//            if (error != nil) {
-//                print(error)
-//            } else {
-//                let httpResponse = response as? HTTPURLResponse
-//                print(httpResponse)
-//            }
-//        })
-//
-//        dataTask.resume()
-//    }
-    //; end of API call
-    
-    func getNearbyPlaces(coordinate: CLLocationCoordinate2D, completion: @escaping () -> ()) {
-        
-        let headers = [
-                  "X-RapidAPI-Host": "trueway-places.p.rapidapi.com",
-                  "X-RapidAPI-Key": "d21d23c616msh0dfcc69840c5774p1c3012jsne9d41c7837b3"
-              ]
-        
-        let apiKey: String = "4cdc43b784690435fdb782327f18f41f"
-        let radius: String = "10000" //in meters?
-        let language: String = "en"
-        
-        let placesURL = URL(string: "https://trueway-places.p.rapidapi.com/FindPlacesNearby?location=\(coordinate.latitude)%2C\(coordinate.longitude)&radius=\(radius)&language=\(language)")!
-        let placesURLConvertible: Alamofire.URLConvertible = placesURL
-    
-        
-        AF.request(placesURLConvertible).responseData { response in
-            switch response.result {
-            case .failure(let error):
-                print(error.localizedDescription)
-            case .success(let data):
-                do {
-                    let placesDataResponseBody = try JSONDecoder().decode(PlacesResponseBody.self, from: data)
-                    let result = placesDataResponseBody.results[0].id
-                    
-                    
-                    print(result)
-                } catch let error {
-                    print(error.localizedDescription)
-                }
-            }
-        }
-    }
-
     func getCoordinateFrom(address: String, completion: @escaping(_ coordinate: CLLocationCoordinate2D?, _ error: Error?) -> () ) {
         CLGeocoder().geocodeAddressString(address) { completion($0?.first?.location?.coordinate, $1) }
         let address = "Rio de Janeiro, Brazil"
-
+        
         getCoordinateFrom(address: address) { coordinate, error in
             guard let coordinate = coordinate, error == nil else { return }
             // don't forget to update the UI from the main thread
@@ -82,9 +23,45 @@ class PlacesManager {
         }
     }
     
-    
+    func getNearbyPlaces(coordinate: CLLocationCoordinate2D) {
+        
+        let headers: HTTPHeaders = [
+            "X-RapidAPI-Host": "trueway-places.p.rapidapi.com",
+            "X-RapidAPI-Key": "d21d23c616msh0dfcc69840c5774p1c3012jsne9d41c7837b3"
+        ]
+        
+        let radius: String = "10000" //i believe this is in meters?
+        let language: String = "en"
+        
+        let placesURL = URL(string: "https://trueway-places.p.rapidapi.com/FindPlacesNearby?location=\(coordinate.latitude)%2C\(coordinate.longitude)&radius=\(radius)&language=\(language)")!
+        let placesURLConvertible: Alamofire.URLConvertible = placesURL
+        
+        
+        // this is going to be a real request, this might put all the returned data in a data structure perhaps?
+        //        AF.request(placesURLConvertible).responseData { response in
+        //            switch response.result {
+        //            case .failure(let error):
+        //                print(error.localizedDescription)
+        //            case .success(let data):
+        //                do {
+        //                    let placesDataResponseBody = try JSONDecoder().decode(PlacesResponseBody.self, from: data)
+        //                    let result = placesDataResponseBody.results[0].id
+        //
+        //
+        //                    print(result)
+        //                } catch let error {
+        //                    print(error.localizedDescription)
+        //                }
+        //            }
+        //        }
+        
+        //for debugging purposes, just to get an overview of the response being returned from the api
+        AF.request(placesURLConvertible, headers: headers).responseData { response in
+            debugPrint(response)
+        }
+    }
 }
-
+    
 struct PlacesResponseBody: Decodable {
     let results: [Result]
 }
