d6f072cfbc9e7dac2581efc8dc9377732f346931
dianarcodes <msdrivas01@gmail.com>
2022-05-18 22:36:25 -0400

    Entered the API

--- BonVoya/PlacesManager.swift
+++ BonVoya/PlacesManager.swift
@@ -10,6 +10,31 @@ import CoreLocation
 
 class PlacesManager {
     
+    //API Places Caller
+    func getNearbyAPI(coordinate: CLLocationCoordinate2D, completion: @escaping () -> ()) {
+        let headers = [
+            "X-RapidAPI-Host": "trueway-places.p.rapidapi.com",
+            "X-RapidAPI-Key": "d21d23c616msh0dfcc69840c5774p1c3012jsne9d41c7837b3"
+        ]
+        
+        let request = NSMutableURLRequest(url: NSURL(string: "https://trueway-places.p.rapidapi.com/FindPlacesNearby?location=\(coordinate.latitude)%2C\(coordinate.longitude)&radius=10000&language=en")! as URL,cachePolicy: .useProtocolCachePolicy, timeoutInterval: 10.0)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+            if (error != nil) {
+                print(error)
+            } else {
+                let httpResponse = response as? HTTPURLResponse
+                print(httpResponse)
+            }
+        })
+
+        dataTask.resume()
+    }
+    //; end of API call
+    
 
     func getCoordinateFrom(address: String, completion: @escaping(_ coordinate: CLLocationCoordinate2D?, _ error: Error?) -> () ) {
         CLGeocoder().geocodeAddressString(address) { completion($0?.first?.location?.coordinate, $1) }
